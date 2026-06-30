4f7304972be40d1e3217024e4f620251563e10c9
PNHD2310 <phamngochongdiem23102003@gmail.com>
2026-06-30 07:00:04 +0700

    Test_Login_Action

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,20 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from utils.config_reader import ConfigReader
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(self.driver, 10)
+        self.wait = WebDriverWait(self.driver, ConfigReader.get_explicit_wait())
 
     def find_element(self, locator):
-        return self.wait.until(lambda d:d.find_element(*locator))
+        try:
+            return self.wait.until(EC.presence_of_element_located(locator))
+        except Exception as e:
+            raise AssertionError(f"Element {locator} not found within timeout: {e}")
+
+    def find_elements(self, locator):
+        return self.wait.until(EC.visibility_of_all_elements_located(locator))
 
     def send_keys(self, locator, text):
         self.find_element(locator).send_keys(text)
@@ -24,7 +28,5 @@ class BasePage:
     def wait_visible(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator))
 
-    def wait_clickable(self, locator):
-        return self.wait.until(EC.element_to_be_clickable(locator))
     def click(self, locator):
-        self.wait_clickable(locator).click()
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
